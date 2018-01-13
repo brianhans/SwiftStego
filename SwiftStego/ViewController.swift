3051b5ac5fb63ed71d85a98f02fa7e8e44934a6e
Brian Hans <wbrianw8@gmail.com>
2018-01-13 13:09:37 -0800

    Initial Commit

---
+++ SwiftStego/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStego
+//
+//  Created by Brian Hans on 1/13/18.
+//  Copyright © 2018 BrianHans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
